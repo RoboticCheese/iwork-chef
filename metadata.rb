c5fd4ec9e72d218df10c2bb3c6970824e9c8a669
Jonathan Hartman <j@p4nt5.com>
2015-04-26 20:56:16 -0700

    Initial commit; freshly-generated cookbook

---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'iwork'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures iwork'
+long_description 'Installs/Configures iwork'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
