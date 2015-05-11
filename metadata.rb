83ead581133ac8d150485ecf61bddc4307031cd0
Jonathan Hartman <j@p4nt5.com>
2015-05-10 21:33:36 -0700

    Update pinned mac-app-store version

--- metadata.rb
+++ metadata.rb
@@ -9,7 +9,7 @@ description      'Installs iWork apps'
 long_description 'Installs iWork apps'
 version          '0.0.1'
 
-depends          'mac-app-store', '~> 0.1'
+depends          'mac-app-store', '~> 1.0'
 
 supports         'mac_os_x'
 # rubocop:enable SingleSpaceBeforeFirstArg
